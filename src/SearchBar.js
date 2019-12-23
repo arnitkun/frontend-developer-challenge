6f35b4b9d5c98de666206e1b9f2d00afb27b2cfe
arnitkun <ankit.nitagartala@gmail.com>
2019-12-24 01:32:26 +0530

    Components starting to communicate.

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -8,38 +8,15 @@ class SearchBar extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    // handleChange(event){
-    //     this.setState({value: event.target.value});
-    // }
-
-    // handleSubmit(event){
-    //     //alert('new link!: ' + this.state.value); //play if first in list else push to list
-    //     let url = this.state.value;
-    //     // console.log(link);
-    //     if(!(url.startsWith("https://www.youtube.com"))){
-    //         alert("Please enter a valid youtube video link.");    
-    //     }
-    //     console.log("the link passed is: "+url);
-
-    //     event.preventDefault();
-    // }
-
-    // componentDidMount() {
-    //     console.log("bar state:" + Object.values(this.state));
-    // }
-
-    // getId(url) {
-    //     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
-    //     const match = url.match(regExp);
-    
-    //     return (match && match[2].length === 11)
-    //       ? match[2]
-    //       : null;
-    // }
-
     handleChange(event){
+        this.props.onLinkSubmission(event.target.value);
+    }
+
+    handleSubmit(event){
+        console.log("running handle-Submit");
         this.props.onLinkSubmission(event.target.value);
     }
     
@@ -49,7 +26,7 @@ class SearchBar extends React.Component {
         const link = this.props.link;
         return (
             <div>
-            <form onSubmit={this.props.handleSubmit}>
+            <form onSubmit={this.handleSubmit}>
             <label>
                 <input type="text" placeholder="Enter the Youtube Link" size="90" onChange={this.handleChange} value={link}/>
                 </label>
