64dd8865d665e3823af3cbf869e0844ef1c8a297
arnitkun <ankit.nitagartala@gmail.com>
2019-12-23 21:41:18 +0530

    Lifted state from searchbar to app

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,35 +1,61 @@
+
 const React = require('react')
+const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
 // import React, {component} from 'react'
  
 class SearchBar extends React.Component {
+
     constructor(props) {
         super(props);
-        this.state = {list:''};
-
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    // handleChange(event){
+    //     this.setState({value: event.target.value});
+    // }
+
+    // handleSubmit(event){
+    //     //alert('new link!: ' + this.state.value); //play if first in list else push to list
+    //     let url = this.state.value;
+    //     // console.log(link);
+    //     if(!(url.startsWith("https://www.youtube.com"))){
+    //         alert("Please enter a valid youtube video link.");    
+    //     }
+    //     console.log("the link passed is: "+url);
+
+    //     event.preventDefault();
+    // }
+
+    // componentDidMount() {
+    //     console.log("bar state:" + Object.values(this.state));
+    // }
+
+    // getId(url) {
+    //     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+    //     const match = url.match(regExp);
+    
+    //     return (match && match[2].length === 11)
+    //       ? match[2]
+    //       : null;
+    // }
 
     handleChange(event){
-        this.setState({value: event.target.value});
+        this.props.onLinkSubmission(event.target.value);
     }
-
-    handleSubmit(event){
-        //alert('new link!: ' + this.state.value); //play if first in list else push to list
-        this.setState({list:this.state.value});
-        // console.log(this.state.value);
-        event.preventDefault();
-    }
+    
 
     render() {
+        console.log(Object.values(this.props));
+        const link = this.props.link;
         return (
-            <form onSubmit={this.handleSubmit}>
+            <div>
+            <form onSubmit={this.props.handleSubmit}>
             <label>
-                <input type="text" placeholder="Enter the Youtube Link" size="90" value={this.state.value} onChange={this.handleChange} />
+                <input type="text" placeholder="Enter the Youtube Link" size="90" onChange={this.handleChange} value={link}/>
                 </label>
-                <input type="submit" value="Get Video" />
+                <input type="submit" value="Get Video"/>
             </form>
-
+            </div>
         );
 
     }
