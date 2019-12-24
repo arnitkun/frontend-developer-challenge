1266edd4f932dd6d7ee679dd15ac14dffe668fdf
arnitkun <ankit.nitagartala@gmail.com>
2019-12-24 19:16:12 +0530

    Added index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Player from './Player';
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Player />, document.getElementById('root1'));
