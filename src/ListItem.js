41dd27dfd710d3189633c53237126e53bb2345d5
arnitkun <ankit.nitagartala@gmail.com>
2019-12-26 18:20:41 +0530

    Updated List-item Component

--- src/ListItem.js
+++ src/ListItem.js
@@ -8,9 +8,10 @@ class ListItem extends React.Component{
     }
     render() {
         console.log(this.props.link);
+        const link = this.props.link;
         return(
             <div className="list-item">
-                <textarea rows="1" columns="30">{this.props.link}</textarea>
+                <textarea rows="1" columns="30" value={link}></textarea>
             </div>
         )
     }
